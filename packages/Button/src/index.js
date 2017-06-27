42f41741f59e2972ab609cff3c31e9f80ac119f0
Ankur Kushwaha <ankur.kushwaha@birdeye.com>
2017-06-27 11:37:04 +0530

    header added

---
+++ packages/Button/src/index.js
@@ -0,0 +1,4 @@
+import { Button } from "./Button";
+module.exports = {
+  Button
+};
---
+++ packages/Header/src/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const H1 = styled.h1`
+background: #1976D2;
+`;
+
+const Header = () => (
+  <div>
+    <H1> Header</H1>
+  </div>
+);
+
+export default Header;
