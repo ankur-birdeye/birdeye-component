deb0bd073e46250851ba7b9251fa0bbe39070277
Ankur Kushwaha <ankur.kushwaha@birdeye.com>
2017-07-04 12:39:09 +0530

    header updated

--- packages/Header/src/Header.js
+++ packages/Header/src/Header.js
@@ -5,6 +5,7 @@ import PropTypes from "prop-types";
 const H1 = styled.h1`
     background: #1976D2;
     padding: 0px 10px;
+    height:60px;
     font-size: 22px;
     font-weight: 300;
     font-style: normal;
@@ -13,13 +14,30 @@ const H1 = styled.h1`
     white-space: nowrap;
     text-overflow: ellipsis;
     color:white;
+    .row{
+      max-width:1200px;
+      margin:0 auto;
+      height:100%;
+    }
+    .right-nav{
+      display: inline-block;
+      float: right;
+      padding: 19px 0;
+    }
+    .icon{
+      margin-left:30px;
+      cursor:pointer;
+    }
+    .icon-user:before{
+      color:white;
+    }
 `;
 
 const Logo = () => (
   <span className="logo-section bf-logo">
-    <a href="/" target="_blank">
+    <a href="/" target="_blank" style={{ verticalAlign: "text-top" }}>
       <img
-        style={{ maxHeight: 50, verticalAlign: "middle" }}
+        style={{ height: "50px", verticalAlign: "middle", paddingTop: "5px" }}
         src="//d3cnqzq0ivprch.cloudfront.net/prod/css/images/logo_white.png"
         alt="BirdEye"
       />
@@ -30,14 +48,22 @@ const Logo = () => (
 const Header = ({ title, children }) => (
   <div>
     <H1 color="green">
-      <Logo /><span
-        style={{
-          position: "relative",
-          top: 2
-        }}
-      >
-        {title}
-      </span>
+      <div className="row">
+        <Logo />
+        <span
+          style={{
+            position: "relative",
+            top: 2
+          }}
+        >
+          {title}
+        </span>
+        <div className="right-nav">
+          <span className="icon icon-app_menu" />
+          <span className="icon icon-user" />
+          <span className="icon icon-hamburger" />
+        </div>
+      </div>
     </H1>
     <div>
       {children}
