98ee662e8be24fdc381b20dab5c589d80c2c8f6a
Ankur Kushwaha <ankur.kushwaha@birdeye.com>
2017-10-13 10:20:42 +0530

    header updated

--- packages/Header/src/Header.js
+++ packages/Header/src/Header.js
@@ -2,6 +2,8 @@ import React from "react";
 import styled from "styled-components";
 import PropTypes from "prop-types";
 
+
+//header updated
 const H1 = styled.h1`
     background: #1976D2;
     padding: 0px 10px;
@@ -34,46 +36,46 @@ const H1 = styled.h1`
 `;
 
 const Logo = () => (
-  <span className="logo-section bf-logo">
-    <a href="/" target="_blank" style={{ verticalAlign: "text-top" }}>
-      <img
-        style={{ height: "50px", verticalAlign: "middle", paddingTop: "5px" }}
-        src="//d3cnqzq0ivprch.cloudfront.net/prod/css/images/logo_white.png"
-        alt="BirdEye"
-      />
-    </a>
-  </span>
+    <span className="logo-section bf-logo">
+        <a href="/" target="_blank" style={{ verticalAlign: "text-top" }}>
+            <img
+                style={{ height: "50px", verticalAlign: "middle", paddingTop: "5px" }}
+                src="//d3cnqzq0ivprch.cloudfront.net/prod/css/images/logo_white.png"
+                alt="BirdEye"
+            />
+        </a>
+    </span>
 );
 
 const Header = ({ title, children }) => (
-  <div>
-    <H1 color="green">
-      <div className="row">
-        <Logo />
-        <span
-          style={{
-            position: "relative",
-            top: 2
-          }}
-        >
-          {title}
-        </span>
-        <div className="right-nav">
-          <span className="icon icon-app_menu" />
-          <span className="icon icon-user" />
-          <span className="icon icon-hamburger" />
-        </div>
-      </div>
-    </H1>
     <div>
-      {children}
+        <H1 color="green">
+            <div className="row">
+                <Logo />
+                <span
+                    style={{
+                        position: "relative",
+                        top: 2
+                    }}
+                >
+                    {title}
+                </span>
+                <div className="right-nav">
+                    <span className="icon icon-app_menu" />
+                    <span className="icon icon-user" />
+                    <span className="icon icon-hamburger" />
+                </div>
+            </div>
+        </H1>
+        <div>
+            {children}
+        </div>
     </div>
-  </div>
 );
 
 Header.propTypes = {
-  children: PropTypes.element,
-  title: PropTypes.string
+    children: PropTypes.element,
+    title: PropTypes.string
 };
 
 export default Header;
